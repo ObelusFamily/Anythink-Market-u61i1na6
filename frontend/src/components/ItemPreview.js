828d6f4681f02353461f6ffba574c9b2741934e3
Zona <echyth@yahoo.com>
2022-11-30 13:38:21 +0000

    fix lint error

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -41,10 +41,10 @@ const ItemPreview = (props) => {
 
       <img
         alt="item"
-        src={item.image ? item.image : "/placeholder.png"}
+        src={ item.image ? item.image : "/placeholder.png" }
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
-        onError={addDefaultSrc}
+        onError={ addDefaultSrc }
       />
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
